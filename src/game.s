4ac855f9fcd9a16c54d33c9b8910e1fed7ffeaf4
Matthew Poole <poolem@tcd.ie>
2024-04-12 17:35:45 +0100

    added comments + video link

--- src/game.s
+++ src/game.s
@@ -1,6 +1,6 @@
 # PASTE LINK TO TEAM VIDEO BELOW
 #
-#
+# https://media.heanet.ie/page/e5892fb5219847fc94f58f274f7a1d77
 
   .syntax unified
   .cpu cortex-m4
@@ -38,29 +38,29 @@ Main:
   LDR     R4, =GPIOE_MODER
   LDR     R5, [R4]                    @ Read ...
 
-  BIC     R5, #(0b11<<(LD3_PIN*2))    @ Modify ...
-  ORR     R5, #(0b01<<(LD3_PIN*2))    @ write 01 to bits 
+  BIC     R5, #(0b11<<(LD3_PIN*2))    @ Clear LED 3 bits
+  ORR     R5, #(0b01<<(LD3_PIN*2))    @ write 01 to bits to set active
 
-  BIC     R5, #(0b11<<(LD4_PIN*2))    @ Modify ...
-  ORR     R5, #(0b01<<(LD4_PIN*2))    @ write 01 to bits 
+  BIC     R5, #(0b11<<(LD4_PIN*2))    @ Clear LED 4 bits
+  ORR     R5, #(0b01<<(LD4_PIN*2))    @ write 01 to bits to set active
   
-  BIC     R5, #(0b11<<(LD5_PIN*2))    @ Modify ...
-  ORR     R5, #(0b01<<(LD5_PIN*2))    @ write 01 to bits 
+  BIC     R5, #(0b11<<(LD5_PIN*2))    @ Clear LED 5 bits
+  ORR     R5, #(0b01<<(LD5_PIN*2))    @ write 01 to bits to set active
 
-  BIC     R5, #(0b11<<(LD6_PIN*2))    @ Modify ...
-  ORR     R5, #(0b01<<(LD6_PIN*2))    @ write 01 to bits 
+  BIC     R5, #(0b11<<(LD6_PIN*2))    @ Clear LED 6 bits
+  ORR     R5, #(0b01<<(LD6_PIN*2))    @ write 01 to bits to set active
 
-  BIC     R5, #(0b11<<(LD7_PIN*2))    @ Modify ...
-  ORR     R5, #(0b01<<(LD7_PIN*2))    @ write 01 to bits 
+  BIC     R5, #(0b11<<(LD7_PIN*2))    @ Clear LED 7 bits
+  ORR     R5, #(0b01<<(LD7_PIN*2))    @ write 01 to bits to set active
 
-  BIC     R5, #(0b11<<(LD8_PIN*2))    @ Modify ...
-  ORR     R5, #(0b01<<(LD8_PIN*2))    @ write 01 to bits 
+  BIC     R5, #(0b11<<(LD8_PIN*2))    @ Clear LED 8 bits
+  ORR     R5, #(0b01<<(LD8_PIN*2))    @ write 01 to bits to set active
   
-  BIC     R5, #(0b11<<(LD9_PIN*2))    @ Modify ...
-  ORR     R5, #(0b01<<(LD9_PIN*2))    @ write 01 to bits 
+  BIC     R5, #(0b11<<(LD9_PIN*2))    @ Clear LED 9 bits
+  ORR     R5, #(0b01<<(LD9_PIN*2))    @ write 01 to bits to set active
 
-  BIC     R5, #(0b11<<(LD10_PIN*2))    @ Modify ...
-  ORR     R5, #(0b01<<(LD10_PIN*2))    @ write 01 to bits 
+  BIC     R5, #(0b11<<(LD10_PIN*2))    @ Clear LED 10 bits
+  ORR     R5, #(0b01<<(LD10_PIN*2))    @ write 01 to bits to set active
 
   STR     R5, [R4]                    @ Write 
 
@@ -113,10 +113,10 @@ Main:
   @   kit) to use the EXTI0 external interrupt signal
   @ Determined by bits 3..0 of the External Interrrupt Control
   @   Register (EXTIICR)
-  LDR     R4, =SYSCFG_EXTIICR1
-  LDR     R5, [R4]
-  BIC     R5, R5, #0b1111
-  STR     R5, [R4]
+  LDR     R4, =SYSCFG_EXTIICR1 @ Load systen configuration to EXTIICR register
+  LDR     R5, [R4]            @ Load External Interrupt control
+  BIC     R5, R5, #0b1111     @ Clear bits 3..0 of External Interrupt control
+  STR     R5, [R4]            @ store new result in SYSCFG_EXTIICR1 
 
   @ Enable (unmask) interrupts on external interrupt Line0
   LDR     R4, =EXTI_IMR
@@ -162,28 +162,28 @@ End_Main:
 random:
   PUSH    {R4-R8,LR}
   @ Save range-1
-  SUB     R7, R0, #1
-  MOV     R8, R1
+  SUB     R7, R0, #1  @ tempRange = range - 1
+  MOV     R8, R1      @ tempSeed = seed
   @ Get amount of bits
-  CMP     R0, #0
+  CMP     R0, #0               @ if range >= 0 {
   BGT     .LgreaterThanZero
-  MOV     R0, #0
-  B       .LendRandom
+  MOV     R0, #0               @ range = 0
+  B       .LendRandom          @ }
 .LgreaterThanZero:
-  MOV     R5, #1
-  MOV     R1, 0b1
-.LbitGetterLoop:
-  CMP     R7, R1
-  BLS     .LendBitGetterLoop     
-  LSL     R1, #1
-  ORR     R1, #1
-  ADD     R5, #1
-  B       .LbitGetterLoop
+  MOV     R5, #1               @ else { 
+  MOV     R1, 0b1              @  numBits = 1; tempMax = 1;
+.LbitGetterLoop:               @  
+  CMP     R7, R1               @  for tempRange < ; tempMax -- {
+  BLS     .LendBitGetterLoop   @ 
+  LSL     R1, #1               @  tempMax = tempMax * 2;
+  ORR     R1, #1               @  
+  ADD     R5, #1               @  numBits++
+  B       .LbitGetterLoop      @ } }
 .LendBitGetterLoop:
   @ Load seed
-  MOV     R1, R8
-  MOV     R6, #0
-  MOV     R0, #0
+  MOV     R1, R8               @ seed = tempSeed
+  MOV     R6, #0               @ bitcount = 0;
+  MOV     R0, #0               @ MaxRange = 0;
 .LprnBitLoop:
   CMP     R6, R5
   BEQ     .LcheckIfBelow
@@ -231,37 +231,39 @@ SysTick_Handler:
 
   PUSH  {R4-R12, LR}
 
-  LDR   R4, =tick
-  LDR   R5, [R4]
-  CMP   R5, #0xffffffff
-  BNE   .LnotMax
-  LDR   R5, =0
+  LDR   R4, =tick       @ tickAddress = tickAddresslocation
+  LDR   R5, [R4]        @ tickNum = tickAddress[0]
+  CMP   R5, #0xffffffff @ if tickNum ==  Max { tickNum = 0 }
+  BNE   .LnotMax        @ 
+  LDR   R5, =0          
 .LnotMax:
-  ADD   R5, #1
-  STR   R5, [R4]
+  ADD   R5, #1          @ tickNumm ++
+  STR   R5, [R4]        @ tickAddress = ticknum
 
 
-  LDR   R4, =blink_countdown        @ if (countdown != 0) {
-  LDR   R5, [R4]                    @
+  LDR   R4, =blink_countdown        @ 
+  LDR   R5, [R4]                    @ countdown = blink_countdown
 .LdontInit:
-  CMP   R5, #0                      @
+  CMP   R5, #0                      @if (countdown != 0) {
   BEQ   .LelseFire                  @
 
-  SUB   R5, R5, #1                  @   countdown = countdown - 1;
-  STR   R5, [R4]                    @
+  SUB   R5, R5, #1                  @   countdown--;
+  STR   R5, [R4]                    @   blink_countDown = countdown
 
   B     .LendIfDelay                @ }
 
 .LelseFire:                         @ else {
 
+@ Set up for game to begin
   LDR R9, =game_ready
   LDR R9, [R9]
   CMP R9, #1
   BNE .LendIfDelay
 
-  LDR     R6, =led_position
+@ Inits starting LED
+  LDR     R6, =led_position         @ startingLEDPos = led_position
   LDR     R3, [R6]
-  LDR     R7, =led_state
+  LDR     R7, =led_state            @ startingLEDState = led_state
   LDR     R8, [R7]
 
   LDR     R4, =GPIOE_ODR            @   Invert LD3
@@ -269,50 +271,57 @@ SysTick_Handler:
   EOR     R5, R3                    @   GPIOE_ODR = GPIOE_ODR ^ (1<<LD3_PIN);
   STR     R5, [R4]                  @ 
 
-  LDR     R4, =blink_countdown      @   countdown = blink_period;
-  LDR     R5, =blink_period         @
+  LDR     R4, =blink_countdown      @  countLength = blink_count
+  LDR     R5, =blink_period         @  countdown = blink_period;
   LDR     R5, [R5]
   STR     R5, [R4]                  @
 
-  LDR     R9, =game_active
-  LDR     R9, [R9]
-  CMP     R9, #0
+  @ Checks if game should be active and resets if a problem arises
+  LDR     R9, =game_active          @ isActive = game_active
+  LDR     R9, [R9]                  @
+  CMP     R9, #0                    @ if isActive == false{start Countdown for next game beginning}
   BEQ     .LstartCountdown
-  CMP     R8, #0
+  CMP     R8, #0                    @ if (isActive && startLEDOn = false ) { reset  }
   BEQ     .LnoReset
 
-  LDR     R9, =play_direction
-  LDR     R9, [R9]
-  CMP     R9, #0
+  @ Checks if Game Should Be reversed and Runs result
+  LDR     R9, =play_direction      @ isReversed = play_direction
+  LDR     R9, [R9]   
+  CMP     R9, #0                   @ if isReversed = true{reverseGame() } 
   BEQ     .LreversedGame
 
-  LSL     R3, R3, #1
-  CMP     R3, #0x8000
-  BLE     .LnoReset
+
+@ Normal function for LED movement
+  LSL     R3, R3, #1               @ currentLEDPos += 1 (LSL moves to next LED)
+  CMP     R3, #0x8000              @ if currentLEDPos >= LED10 {reset to LED 3}
+  BLE     .LnoReset                @ LEDOff()
   MOV     R3, #0x100
-  B       .LnoReset
+  B       .LnoReset                @ LEDOff()
   
 .LreversedGame:
-  LSR     R3, R3, #1
-  CMP     R3, #0x100
-  BGE     .LnoReset
+@ Reverse function for LED movement
+  LSR     R3, R3, #1              @ currentLEDPos += 1 (LSL moves to next LED)
+  CMP     R3, #0x100              @ if currentLEDPos < LED3 {reset to LED 10}
+  BGE     .LnoReset               @ LEDOff()
   MOV     R3, #0x8000
 
 .LnoReset:
-  RSB     R8, R8, #1
-  STR     R8, [R7]
-  STR     R3, [R6]
+@ Turns previously lit LED off
+  RSB     R8, R8, #1              @ currentLEDOn = false
+  STR     R8, [R7]                @ led_state = currentLEDOn
+  STR     R3, [R6]                @ led_position = currentLEDPos
   B       .LendIfDelay
 
 .LstartCountdown:
-  LDR     R8, =game_start_count
-  LDR     R9, [R8]
-  SUB     R9, R9, #1
-  STR     R9, [R8]
-  CMP     R9, #1
+@ Counts down to start of game 
+  LDR     R8, =game_start_count   @ startCountLen = game_start_count
+  LDR     R9, [R8]                
+  SUB     R9, R9, #1              @ startCountLen -= 1
+  STR     R9, [R8]                @ game_start_count = startCountLen
+  CMP     R9, #1                  @ if startCountLen != 1 {endGame()}
   BNE     .LendIfDelay
-  LDR     R8, =game_active
-  STR     R9, [R8]
+  LDR     R8, =game_active        @ else {gameActive = game_active}
+  STR     R9, [R8]                @
 
 .LendIfDelay:                       @ }
 
@@ -342,63 +351,63 @@ EXTI0_IRQHandler:
   MOV     R5, #(1<<0)                 @
   STR     R5, [R4]                    @
   
-  LDR     R9, =game_ready
-  LDR     R10, [R9]
-  CMP     R10, #0
+  LDR     R9, =game_ready             @ gameReadyCheck = gameReadyLocation
+  LDR     R10, [R9]                   @ isGameReadyReg = gameReadyCheck
+  CMP     R10, #0                     @ if isGameReadyTemp == 0 {generateRandomNum()}
   BEQ     .LgenerateRandoms
 
-  LDR     R6, =led_position
-  LDR     R7, [R6]
-  LDR     R8, =win_led
-  LDR     R8, [R8]
-  CMP     R7, R8
+  LDR     R6, =led_position           @ tempLED = LED_PositionLocation
+  LDR     R7, [R6]                    @ LEDValue = tempLED[0]
+  LDR     R8, =win_led                @ tempValue = win_LEDLocation
+  LDR     R8, [R8]                    @ tempValue = tempValue[0]
+  CMP     R7, R8                      @ if tempValue != LEDValue (fail)
   BNE     .Lfail
-  MOV     R7, #0x800
-  STR     R7, [R6]
+  MOV     R7, #0x800                  @ Light Loss LED
+  STR     R7, [R6]                    @ Store LED positon
   B       .Lexit
 .Lfail:
-  MOV     R7, #0x200
-  STR     R7, [R6]
+  MOV     R7, #0x200                  @ Light Win LED
+  STR     R7, [R6]                    @ Store LED position
 .Lexit:
-  MOV     R9, #0
-  LDR     R10, =game_active
-  STR     R9, [R10]
+  MOV     R9, #0                      @ gameReadyCheck = false;
+  LDR     R10, =game_active           @ gameActiveTemp = gameActiveLocation
+  STR     R9, [R10]                   @ store gameActiveCheck in memory
   B     .LendSub
 
 .LgenerateRandoms:
   @initalize count
-  MOV   R0, #50
-  LDR   R2, =tick
-  LDR   R1, [R2]
-  BL    random
-  ADD   R0, R0, #20
-  LDR   R1, =blink_period
+  MOV   R0, #50                       @ RNGrange = 50
+  LDR   R2, =tick                     @ tickNumTemp = tickNumMem
+  LDR   R1, [R2]                      @ seed = tickNumTenp
+  BL    random                        @ RNG(range, seed )
+  ADD   R0, R0, #20                   @ randomNum += 20
+  LDR   R1, =blink_period             
   STR   R0, [R1]
   @initalize win_led
-  MOV     R0, #8
-  LDR     R1, =tick
-  LDR     R1, [R1]
-  BL      random
+  MOV     R0, #8                      @ range = 8
+  LDR     R1, =tick                   
+  LDR     R1, [R1]                    @ seed = numticks
+  BL      random                      @ random(range, seed)
   @correct to 8-15 range reqd
-  ADD     R0, #8
-  MOV     R1, #1
-  LSL     R1, R0
-  LDR     R2, =win_led
-  STR     R1, [R2]
-  LDR     R2, =led_position
-  STR     R1, [R2]
+  ADD     R0, #8                      @ randomNum += 8
+  MOV     R1, #1                      @ Win_LEDLocation = 1 
+  LSL     R1, R0                      @ Win_LEDLocation = 2 ^ randomNum (= LED (randomNum))
+  LDR     R2, =win_led                @ Win_LEDMemTemp = Win_LEDMem
+  STR     R1, [R2]                    @ Win_LEDMemTemp[0] = Win_LEDLocatiom
+  LDR     R2, =led_position           @ Win_LEDMemTemp = Win_LEDPosMem
+  STR     R1, [R2]                    @ Win_LEDMemTemp[0] = Win_LEDLocation
 
   @initialize play_direction
-  MOV     R0, #2
-  LDR     R1, =tick
-  LDR     R1, [R1]
-  BL      random
-  LDR     R1, =play_direction
-  STR     R0, [R1]
+  MOV     R0, #2                      @ range = 2
+  LDR     R1, =tick                    
+  LDR     R1, [R1]                    @ seed = numTicks
+  BL      random                      @ random(range, seed)
+  LDR     R1, =play_direction         @ PlayDirectionTemp = PlayDirectionTempMem
+  STR     R0, [R1]                    @ PlatDirectionMem[0] = randomNum 
 
-  MOV     R10, #1
-  LDR     R9, =game_ready
-  STR     R10, [R9]
+  MOV     R10, #1                     @ gameReadyTemp = true
+  LDR     R9, =game_ready             @
+  STR     R10, [R9]                   @ gameReadyFlag = gameReadyTemp
 .LendSub:
 
   @ Return from interrupt handler
